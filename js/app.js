ebe1c8365e28f5dcd59ccf1991ee945455d758fb
McDaniels Okoro <mcdaniels.okoro@gmail.com>
2018-03-01 17:27:01 +0100

    Added Site Contents
    * Changed some jumbotron images
    * Changed horizontal rule css display
    * Added contact forms
    * Styling contact forms yet to come

--- js/app.js
+++ js/app.js
@@ -54,3 +54,29 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
+
+    /*
+    //Scroll reveal behaviour function 
+    // GENERAL SETTING
+    window.sr = ScrollReveal({ reset: true });
+
+    // Custom Settings
+    sr.reveal('.foo-1', { duration: 200 });
+
+    sr.reveal('.foo-2', {
+        origin: 'right',
+        duration: 2000
+    });
+
+    sr.reveal('.foo-3', {
+        rotate: { x: 100, y: 100, z: 0 },
+        duration: 1000
+    });
+
+    sr.reveal('.foo-4', {
+        viewFactor: 0.5
+    });
+
+    sr.reveal('.foo-5', {
+        duration: 200
+    });*/
